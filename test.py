e2fb225f9de2260dc22e88c8c133dac869fa1e89
AmineWallah <lionannaba@gmail.com>
2026-07-17 21:37:26 +0100

    Remove test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-d = np.load('/home/amine/PycharmProjects/osunator/features/0a6f180152473b4a863afb0c5f9f4fec.npz')
-p = d['approach_progress']
-print(p.min(), p.max(), np.isnan(p).any(), (p == 0).mean(), (p == 1).mean())
